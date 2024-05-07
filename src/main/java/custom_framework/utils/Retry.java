60a6c1a24fc389823321b994e337124e77d329aa
Quality Assurance Automation Engineer <inanikolina@gmail.com>
2024-05-07 22:55:25 +0200

    Readme update, test_test.properties file added

--- src/main/java/custom_framework/utils/Retry.java
+++ src/main/java/custom_framework/utils/Retry.java
@@ -6,12 +6,12 @@ import org.testng.ITestResult;
 public class Retry implements IRetryAnalyzer {
 
     private int count = 0;
-    private static int maxTry = 1;
+    private static final int NUMBER_OF_TRIES = 1;
 
     @Override
     public boolean retry(ITestResult iTestResult) {
         if (!iTestResult.isSuccess()) {                      //Check if test not succeed
-            if (count < maxTry) {                            //Check if maxtry count is reached
+            if (count < NUMBER_OF_TRIES) {                   //Check if NUMBER_OF_TRIES count is reached
                 count++;                                     //Increase the maxTry count by 1
                 iTestResult.setStatus(ITestResult.FAILURE);  //Mark test as failed
                 return true;                                 //Tells TestNG to re-run the test
@@ -23,5 +23,4 @@ public class Retry implements IRetryAnalyzer {
         }
         return false;
     }
-
 }
