e76e9a9b7989fa1ea0c05699e1de37ce2d2a326a
Quality Assurance Automation Engineer <inanikolina@gmail.com>
2024-05-08 20:06:38 +0200

    remote driver implemented standalone Selenium Grid

--- src/main/java/custom_framework/utils/FrameworkSetup.java
+++ src/main/java/custom_framework/utils/FrameworkSetup.java
@@ -2,16 +2,22 @@ package custom_framework.utils;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v124.log.Log;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
@@ -38,9 +44,11 @@ public class FrameworkSetup {
     }
 
     @BeforeMethod(alwaysRun = true)
-    public void startDriver() {
+    public void startDriver() throws MalformedURLException {
         if (settings.getProperty("run").equalsIgnoreCase("local")) {
             startLocalDriver();
+        } else if (settings.getProperty("run").equalsIgnoreCase("remote")) {
+            initializeRemoteDriver();
         }
     }
 
@@ -68,6 +76,27 @@ public class FrameworkSetup {
             throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeout));
+        DriverThreadLocal.setDriver(driver);
+    }
+
+    private void initializeRemoteDriver() throws MalformedURLException {
+        WebDriver driver;
+        Capabilities options;
+        if (browser.equalsIgnoreCase("chrome")) {
+            options = getChromeOptions();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            options = new FirefoxOptions();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            options = new EdgeOptions();
+        } else if (browser.equalsIgnoreCase("HeadlessChrome")) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--incognito", "--headless", "--disable-extensions", "--window-size=1920,1080");
+            options = chromeOptions;
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+        driver = new RemoteWebDriver(new URL("http://192.168.1.100:4444/wd/hub"), options);
+        driver.manage().window().maximize();
         DriverThreadLocal.setDriver(driver);
     }
 
