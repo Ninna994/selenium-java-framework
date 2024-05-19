a08c1879494d8884d3afea0e6442acfb3c1ab29c
Quality Assurance Automation Engineer <inanikolina@gmail.com>
2024-05-19 22:07:41 +0200

    CheckboxesTest.java finished

---
+++ src/main/java/custom_framework/apps/theinternet/test_cases/CheckboxesTest.java
@@ -0,0 +1,42 @@
+package custom_framework.apps.theinternet.test_cases;
+
+import custom_framework.apps.theinternet.page_flows.TheInternetFlow;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class CheckboxesTest extends TheInternetFlow {
+    @Test
+    @Description("Check all checkboxes")
+    @Owner("Ninna994")
+    public void testCheckAll() {
+        navCheckboxes();
+        verifyPageTitle("Checkboxes");
+
+        List<WebElement> listOfCheckboxes = driver().findElements(theCheckbox());
+        for (WebElement checkbox : listOfCheckboxes) {
+            if (!checkbox.isSelected()) {
+                checkbox.click();
+            }
+        }
+
+    }
+
+    @Test
+    @Description("Uncheck all checkboxes")
+    @Owner("Ninna994")
+    public void testUncheckAll() {
+        navCheckboxes();
+        verifyPageTitle("Checkboxes");
+
+        List<WebElement> listOfCheckboxes = driver().findElements(theCheckbox());
+        for (WebElement checkbox : listOfCheckboxes) {
+            if (checkbox.isSelected()) {
+                checkbox.click();
+            }
+        }
+    }
+}
