b68e876a062da1c9189ca7d0fc20348086f1f18a
Quality Assurance Automation Engineer <inanikolina@gmail.com>
2024-05-22 01:38:46 +0200

    Geolocation broken test added

--- src/main/java/custom_framework/apps/theinternet/test_cases/GeolocationTest.java
+++ src/main/java/custom_framework/apps/theinternet/test_cases/GeolocationTest.java
@@ -1,15 +1,23 @@
 package custom_framework.apps.theinternet.test_cases;
 
 import custom_framework.apps.theinternet.page_flows.TheInternetFlow;
+import custom_framework.objects.CountryLocation;
 import io.qameta.allure.Description;
 import io.qameta.allure.Owner;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GeolocationTest  extends TheInternetFlow {
-    @Test
-    @Description("")
+public class GeolocationTest extends TheInternetFlow {
+    @Test(groups = "Broken")
+    @Description("Emulate geolocation using CDP")
     @Owner("Ninna994")
-    public void test() {
+    public void testGeolocationChanges() {
+        CountryLocation brazil = CountryLocation.CANADA;
+        mockGeolocation(brazil.getLatitude(), brazil.getLongitude(), 1);
+        navGeolocation();
+        verifyPageTitle("Geolocation");
+        click(theButtonWithText("Where am I?"));
+        Assert.assertEquals(verifyText(theLatitudeValue()), String.valueOf(brazil.getLatitude()));
 
     }
 }
